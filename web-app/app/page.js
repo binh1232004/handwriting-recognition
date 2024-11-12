0bd5be0c7bfaa96d7852e6f4f0b9229b01e0d100
Binh <binhwork245@gmail.com>
2024-11-12 13:59:56 +0700

    chore: add zip dependency

--- web-app/app/page.js
+++ web-app/app/page.js
@@ -15,15 +15,9 @@ export default function Home() {
   <div className="w-full h-[100vh] flex flex-col justify-center items-center" 
      style={{ backgroundImage: "url('http://localhost:3000/background.jpg')", backgroundSize: "cover" }}>
     <form className="flex flex-col rounded space-y-6 sm:w-1/2">
-      <Upload maxCount={1}>
-        <Button icon={<UploadOutlined/>}>Click to upload image (Max: 1)</Button>
+      <Upload maxCount={1} className="w-full">
+        <Button icon={<UploadOutlined/>}>Click to upload image (Max: 10)</Button>
       </Upload>
-      <Input 
-        size="large" 
-        placeholder="Enter your path" 
-        prefix={<FileMarkdownOutlined />} 
-        onChange={handlePathVaultChange}
-      />
       <Button type="primary" onClick={handleButtonClick}>Save to your obsidian vault</Button>
     </form>
   </div>
